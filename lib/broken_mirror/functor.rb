024b4a32099e257a171b3e5a30aa71b650dd2020
Shamash2014 <warp.buddhist@gmail.com>
2016-07-13 23:47:15 +0300

    Hello travis CI

--- lib/broken_mirror/functor.rb
+++ lib/broken_mirror/functor.rb
@@ -1,7 +1,7 @@
 module BrokenMirror
   # Functor provide only one method -> fmap
   # Functor's fmap curried by default
-  # Functor behaviour specified only for callable iterface
+  # Functor behaviour specified only for callable interface
   # Morphism must be callable and return value
   module Functor
     # fmap :: (a -> b) -> Proc a -> Proc b
